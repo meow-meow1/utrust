7e5d4248010f28ca64c767debc45200302a55024
meow-meow1 <zamoozyaka@gmail.com>
2022-09-13 21:28:49 +0300

    moving api config to settings

--- cypress-automation/cypress/support/commands.js
+++ cypress-automation/cypress/support/commands.js
@@ -1,28 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const {MailSlurp} = require('mailslurp-client');
+import {apiKey} from "../../config"
+
 
 Cypress.Commands.add('isVisible', selector => {
     cy.get(selector).should('be.visible');
@@ -46,24 +24,12 @@ Cypress.Commands.add('setResolution', size => {
     }
 })
 
-// // TODO maybe create one function intstead of two with flag like register=true? but then the function name will be confusing ><
-// Cypress.Commands.add('register', (username, password) => {
-//     // cy.get('.header__optionLineTwo').click();
-//     cy.contains('Sign-in').should('be.visible');
-//     cy.get('#email').type(username)
-//     cy.get('#password').type(password)
-//     cy.get('.login__registerButton').click();
-// })
-
+// TODO: add API login funciton
 Cypress.Commands.add('logInWithApi', (username, password) => {
-
 })
 
 
-const {MailSlurp} = require('mailslurp-client');
 // set your api key with an environment variable `CYPRESS_API_KEY` or configure using `env` property in config file
-// (cypress prefixes environment variables with CYPRESS)
-const apiKey = 'e5c7cbd6be09cd3da0884e9d6c07c9894b8bbf36f8774937a99dd4b4f1076b99'
 const mailslurp = new MailSlurp({apiKey});
 
 Cypress.Commands.add("createInbox", () => {
